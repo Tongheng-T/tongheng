444c033d3df0c04fceaf4701e37a4ad5d5284546
Tongheng-T <tonghengit@gmail.com>
2021-10-14 18:38:55 +0700

    h

---
+++ student_project_files_php/resources/functions.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+
+// helper function
+
+function redirect($location){
+    header("Location: $location");
+}
+
+function query($sql){
+    global $connection;
+    return mysqli_query($connection, $sql);
+}
+
+function confirm($result){
+    global $connection;
+    if(!$result) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+}   
+
+function edcape_string($string){
+    global $connection;
+    return mysqli_real_escape_string($connection, $string);
+}
+
+
+function fetch_array($result){
+
+    return ($row = mysqli_fetch_array($result));
+}
+
+// get products 
+
+function get_products() {
+
+    $query=  query("SELECT * FROM products");
+    confirm($query);
+    
+    while($row = fetch_array($query)){
+        
+        echo $row['product_price'];
+    }
+}
+
+?>
